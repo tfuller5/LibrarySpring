9281dd493dcb32ec3bea3ef7d3938810ff84d553
Tom Fuller <tom80f@gmail.com>
2022-07-20 19:39:14 +0100

    lesson :)

--- src/main/java/com/example/demo/MinalApplication.java
+++ src/main/java/com/example/demo/MinalApplication.java
@@ -2,13 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
 public class MinalApplication {
 	public static void main(String[] args) {
-
-		SpringApplication.run(MinalApplication.class, args);
+		ConfigurableApplicationContext app =  SpringApplication.run(MinalApplication.class, args);
 
 		for (int i =0; i<100; i++) {
 			System.out.println("code");

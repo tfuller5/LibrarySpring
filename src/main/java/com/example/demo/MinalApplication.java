5bfe0e398ffe263c5b8d78a1613c6824080426c8
Tom Fuller <tom80f@gmail.com>
2022-07-20 18:45:37 +0100

    important thing to note is that these changes are on branch 222

--- src/main/java/com/example/demo/MinalApplication.java
+++ src/main/java/com/example/demo/MinalApplication.java
@@ -7,7 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MinalApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(MinalApplication.class, args);
+
+		for (int i =0; i<100; i++) {
+			System.out.println("code");
+		}
 	}
+
+
+
 }
 
